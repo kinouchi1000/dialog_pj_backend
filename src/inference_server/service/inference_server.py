bf8d0a9bfbcf2aa929a52ef81b2e3ee3cea8f1a0
rmaejima <ryo.maejii@gmail.com>
2022-06-02 10:22:18 +0900

    Fix response dict

--- src/inference_server/service/inference_server.py
+++ src/inference_server/service/inference_server.py
@@ -2,8 +2,12 @@ import logging
 from concurrent import futures
 
 import grpc
-from common.constants import (INFERENCE_SERVER_IP, INFERENCE_SERVER_PORT,
-                              MAX_WORKER, REPLY_HISTORY_LIMIT)
+from common.constants import (
+    INFERENCE_SERVER_IP,
+    INFERENCE_SERVER_PORT,
+    MAX_WORKER,
+    REPLY_HISTORY_LIMIT,
+)
 from google.protobuf import json_format
 from grpc_service import inference_server_pb2, inference_server_pb2_grpc
 from grpc_service.dialog_server_pb2 import Reply
@@ -16,14 +20,13 @@ class InferenceServicer(inference_server_pb2_grpc.InferenceServiceServicer):
     def __init__(self) -> None:
         self.controller = Controller()
 
-
     def GetReply(self, request, con):
 
         comment_list = []
-        histories_dict = json_format.MessageToDict(request.comments)
+        histories_dict = json_format.MessageToDict(request)
 
         for comment in histories_dict["comments"]:
-            comment_list.append(self.get_dict(comment["speakerId"],comment["comment"]))
+            comment_list.append(self.get_dict(comment["speakerId"], comment["comment"]))
 
         comment_list = list(filter(None, comment_list))
 
@@ -31,16 +34,18 @@ class InferenceServicer(inference_server_pb2_grpc.InferenceServiceServicer):
         return Reply(comment=reply)
 
     @staticmethod
-    def get_dict(id:str, comment:str):
-        if id != None and comment !=None and id != "" and comment != "":
-            return {'spk':id, 'utt':comment}
+    def get_dict(id: str, comment: str):
+        if id != None and comment != None and id != "" and comment != "":
+            return {"spk": id, "utt": comment}
         else:
             return None
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=MAX_WORKER))
-    inference_server_pb2_grpc.add_InferenceServiceServicer_to_server(InferenceServicer(), server)
+    inference_server_pb2_grpc.add_InferenceServiceServicer_to_server(
+        InferenceServicer(), server
+    )
 
     server.add_insecure_port(f"{INFERENCE_SERVER_IP}:{INFERENCE_SERVER_PORT}")
     server.start()
